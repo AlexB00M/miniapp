608bd7720d86c31044b739b33583102a1ea4588e
AlexB00M <117171237+AlexB00M@users.noreply.github.com>
2024-12-20 21:07:30 +0300

    first

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    EEEEEEEEEEE
+  </div>
+</template>
---
+++ nuxt.config.js
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devServer: {
+    host: "192.168.1.2",
+    port: "20"
+  },
+
+  compatibilityDate: "2024-12-20",
+})
